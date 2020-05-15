fd4fff51f775c94350ca66b4f5e49d16449d015b
sailyw <1152827086@qq.com>
2020-05-15 17:46:39 +0800

    编辑页面完善

--- src/pages/Edit/index.js
+++ src/pages/Edit/index.js
@@ -1,7 +1,40 @@
+import blog from "../../api/blog";
 export default {
   data() {
     return {
-      msg: "welcome",
+      blogId: null,
+      title: "",
+      description: "",
+      content: "",
+      atIndex: false,
     };
+  },
+  //获取博客当前信息
+  created() {
+    this.blogId = this.$route.params.blogId;
+    blog.getDetail({ blogId: this.blogId }).then((res) => {
+      this.title = res.data.title;
+      this.description = res.data.description;
+      this.content = res.data.content;
+      this.atIndex = res.data.atIndex;
+    });
+  },
+  methods: {
+    onEdit() {
+      blog
+        .updateBlog(
+          { blogId: this.blogId },
+          {
+            title: this.title,
+            content: this.content,
+            description: this.description,
+            atIndex: this.atIndex,
+          }
+        )
+        .then((res) => {
+          this.$message.success(res.msg);
+          this.$router.push({ path: `/detail/${res.data.id}` });
+        });
+    },
   },
 };
