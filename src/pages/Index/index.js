b3865257057a27c5873e5847c03cb99b61282f1b
sailyw <1152827086@qq.com>
2020-05-15 14:01:42 +0800

    首页功能搭建

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,27 +1,35 @@
-import request from "../../helpers/request.js";
-import auth from "../../api/auth";
 import blog from "../../api/blog";
-
-window.request = request;
-window.auth = auth;
-window.blog = blog;
 
 export default {
   data() {
     return {
-      msg: "welcome",
+      blogs: [],
+      total: 0, //一共多少条数据
+      page: 1, //当前页
     };
   },
+  // 获取数据
+  created() {
+    this.page = parseInt(this.$route.query.page) || 1;
+    blog.getIndexBlogs({ page: this.page }).then((res) => {
+      console.log(res);
+      this.blogs = res.data;
+      this.total = res.total;
+      this.page = res.page;
+    });
+  },
   methods: {
-    onClick1() {
-      this.$message.error("错了哦，这是一条错误消息");
-    },
-    onClick2() {
-      this.$alert("这是一段内容", "标题名称", {
-        confirmButtonText: "确定",
-        callback: () => {
-          this.$message.success("点了确定");
-        },
+    onPageChange(newPage) {
+      console.log(newPage);
+      blog.getIndexBlogs({ page: newPage }).then((res) => {
+        console.log(res);
+        this.blogs = res.data;
+        this.total = res.total;
+        this.page = res.page;
+        // 刷新时还会再之前的那一页
+        // this.$router.push({ path: `/?page=${newPage}` });
+        // 这样是不行的 我们需要在一开始的时候就进行测试
+        this.$router.push({ path: "/", query: { page: newPage } });
       });
     },
   },
